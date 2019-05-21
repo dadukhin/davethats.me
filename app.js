49bb1c1ec2203cb37b587559f2be6d1ef7268d22
David-Solodukhin <david.solodukhin@gmail.com>
2019-05-21 04:30:29 +0000

    more tidey

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var fs = require('fs')
 var https = require('https')
 var http = require('http')
-
+var bodyParser = require('body-parser');
 //var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
 
@@ -17,17 +17,18 @@ var globalViews = 0;
 //app.listen(app.get('port'));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
+//app.use(bodyParser.urlencoded({ extended: true }));
 
 http.createServer(function (req, res) {
   //res.writeHead(200, {'Content-Type': 'text/html'});
   //res.write('Hello World!');
+  logRequest(req, false);
   res.writeHead(302, {'Location':  'https:\/\/'+ req.headers.host + req.url});
   res.end();
-  logRequest(req, false);
 }).listen(80);
 
 
@@ -52,7 +53,6 @@ function logRequest(req, secure) {
     return;
   }
   var logger;
-  console.log(req.url);
   if (req.headers.host === 'github.davidthats.me') {
     globalViews++;
     logger = fs.createWriteStream('public/gitlog.txt', {
@@ -71,8 +71,9 @@ function logRequest(req, secure) {
   logger.write(secure ? '**SECURE REQUEST:\n' : '::INSECURE REQUEST:\n');
   logger.write('URL: ' + 'http(s):\/\/' + req.headers.host + req.url + "\n");
   logger.write((new Date()).toJSON().slice(0, 19).replace(/[-T]/g, ':')+"\n");
+  logger.write(req.method+"\n");
   logger.write(JSON.stringify(req.headers, null, 2) + "\n");
-  logger.write(req.ip + "\n");
+  logger.write(((req.ip === undefined) ? (req.connection.remoteAddress) : (req.ip)) + "\n");
   logger.write(JSON.stringify(req.cookies) + "\n");
   logger.write(JSON.stringify(req.body) + "\n");
 
